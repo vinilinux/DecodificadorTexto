2148f4bf3d3f99c9602a45bd41046cb0308a5c9f
vinicius lima <vinicius.limalveslinux@gmail.com>
2024-08-10 12:05:37 -0300

    criado as funções criptografar e descriptar que somente mexe na visibilidade dos elementos

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function descriptar(){
+    visibilidadeDosElementos();
+
+}
+
+function criptografar() {
+    visibilidadeDosElementos();
+}
+
+function visibilidadeDosElementos() {
+    document.getElementById("buttonCopiar").style.visibility = "visible";
+    document.getElementById("divSemTextoCriptografado").style.visibility = "hidden";
+}
